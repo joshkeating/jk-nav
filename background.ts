97fe3dd87fb2d8ed84652d046b08e73c23cfe067
joshkeating <josh.keating7@gmail.com>
2018-03-07 23:45:59 -0800

    alright time to start this for real

---
+++ background.ts
@@ -0,0 +1,11 @@
+
+
+// background goes here
+
+// use this to To handle key chords 
+// on j or k press, move to next element in the page link list
+
+// var elems = document.getElementsByTagName("iframe");
+
+// need a browser action to select wether it should be enabled on current page
+
