db52bcc089a94b251a3b4ddc0b4c071adc1da7d5
Student User <studentuser@D-173-250-172-134.dhcp4.washington.edu>
2018-03-08 17:19:36 -0800

    documentation msg

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -2,6 +2,8 @@ import { Page } from './page'
 
 // this is going to be the event page
 
+// should stay open until not needed
+
 // background goes here
 // use this to To handle key chords 
 // on j or k press, move to next element in the page link list
@@ -14,6 +16,7 @@ import { Page } from './page'
 // content script that sends a message to event page as it traverses dom
 
 
+// create array of sites
 
 // get current tab
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
